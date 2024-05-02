88e111fcf181de7ce8b2716cb0485d55a02f759a
25Shawn <112091876+25Shawn@users.noreply.github.com>
2024-05-02 08:55:59 -0400

    Update utilisateurRoutes.js

--- src/routes/utilisateurRoutes.js
+++ src/routes/utilisateurRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();    
 const usager = require('../controllers/utilisateurController');
+const {logger} = require('../middlewares/ErreurLogs');
 
+router.use(logger);
 // ajouter un utilisateur
 router.post('/ajouterUtilisateur', (req,res) => {
     usager.AjouterUtilisateur(req, res);
