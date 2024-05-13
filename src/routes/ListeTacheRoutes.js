364a06dace7fb699cb9520293faccaf42eee5f36
25Shawn <112091876+25Shawn@users.noreply.github.com>
2024-05-13 16:20:15 -0400

    Update ListeTacheRoutes.js

--- src/routes/ListeTacheRoutes.js
+++ src/routes/ListeTacheRoutes.js
@@ -4,13 +4,12 @@ const listeTache = require('../controllers/ListeTacheController');
 const authentification = require('../middlewares/authentification');
 const {logger} = require('../middlewares/ErreurLogs');
 
-// Pour toutes les erreurs
 router.use(logger);
 
 // affiche la liste des tâches incomplètes
 router.get('/afficherListeTache',authentification, (req, res) => {
 
-    listeTache.RequeteafficherListeTacheIncomplete(req, res);
+    listeTache.afficherListeTacheIncomplete(req, res);
 });
 
 // affiche la liste des tâches complètes
